715c8b9b6a22bfc2c6d899212bcb13c7c8824db6
rwithik <rwithik@gmail.com>
2020-01-12 13:21:26 +0530

    chabge enailer

--- methods/authentication/index.js
+++ methods/authentication/index.js
@@ -5,7 +5,7 @@ const models = require("../../models");
 const Promise = require("bluebird");
 const key = require("../../config/api.json").API_SECRET;
 const { sequelize } = require("../../models");
-const otpGenerator = require('otp-generator');
+const otpGenerator = require("otp-generator");
 const nodemailer = require("nodemailer");
 var authenticationMethods = {};
 const options = {
@@ -13,110 +13,112 @@ const options = {
   alphabets: false,
   upperCase: false,
   specialChars: false
-}
+};
 
 authenticationMethods.registerAdmin = function(info) {
   return new Promise(function(resolve, reject) {
-    bcrypt.hash(info.password, saltRounds).then(hash=>{
-      return sequelize
-      .transaction(function(t) {
-        var user = {}
-       user.username = info.username
-       user.password = hash
-       user.type = 1
-      //  console.log(hash)
-        return models.user
-          .create(user, { transaction: t })
-          .then(function(user) {
-          // console.log(user)
-           
+    bcrypt
+      .hash(info.password, saltRounds)
+      .then(hash => {
+        return sequelize
+          .transaction(function(t) {
+            var user = {};
+            user.username = info.username;
+            user.password = hash;
+            user.type = 1;
+            //  console.log(hash)
+            return models.user
+              .create(user, { transaction: t })
+              .then(function(user) {
+                // console.log(user)
+              })
+              .catch(function(err) {
+                reject({ success: err });
+              });
+          })
+          .then(function(result) {
+            console.log("SUCCESS");
+            resolve({ success: true });
           })
           .catch(function(err) {
-            reject({ success: err });
+            reject({ success: "false3" });
           });
       })
-      .then(function(result) {
-        console.log("SUCCESS")
-        resolve({ success: true });
-      })
-      .catch(function(err) {
-        reject({ success: "false3" });
+      .catch(err => {
+        reject({ success: "false4" });
       });
-    })
-    .catch(err=>{
-      reject({success : "false4"});
-    })
   });
 };
 
 authenticationMethods.registerHOD = function(info) {
   return new Promise(function(resolve, reject) {
-    bcrypt.hash(info.password, saltRounds).then(hash=>{
-      return sequelize
-      .transaction(function(t) {
-        var user = {}
-       user.username = info.username
-       user.password = hash
-       user.type = 2
-      //  console.log(hash)
-        return models.user
-          .create(user, { transaction: t })
-          .then(function(user) {
-          // console.log(user)
-           
+    bcrypt
+      .hash(info.password, saltRounds)
+      .then(hash => {
+        return sequelize
+          .transaction(function(t) {
+            var user = {};
+            user.username = info.username;
+            user.password = hash;
+            user.type = 2;
+            //  console.log(hash)
+            return models.user
+              .create(user, { transaction: t })
+              .then(function(user) {
+                // console.log(user)
+              })
+              .catch(function(err) {
+                reject({ success: err });
+              });
+          })
+          .then(function(result) {
+            console.log("SUCCESS");
+            resolve({ success: true });
           })
           .catch(function(err) {
-            reject({ success: err });
+            reject({ success: "false3" });
           });
       })
-      .then(function(result) {
-        console.log("SUCCESS")
-        resolve({ success: true });
-      })
-      .catch(function(err) {
-        reject({ success: "false3" });
+      .catch(err => {
+        reject({ success: "false4" });
       });
-    })
-    .catch(err=>{
-      reject({success : "false4"});
-    })
   });
 };
 
 authenticationMethods.registerAdvisor = function(info) {
   return new Promise(function(resolve, reject) {
-    bcrypt.hash(info.password, saltRounds).then(hash=>{
-      return sequelize
-      .transaction(function(t) {
-        var user = {}
-       user.username = info.username
-       user.password = hash
-       user.type = 3
-      //  console.log(hash)
-        return models.user
-          .create(user, { transaction: t })
-          .then(function(user) {
-          // console.log(user)
-           
+    bcrypt
+      .hash(info.password, saltRounds)
+      .then(hash => {
+        return sequelize
+          .transaction(function(t) {
+            var user = {};
+            user.username = info.username;
+            user.password = hash;
+            user.type = 3;
+            //  console.log(hash)
+            return models.user
+              .create(user, { transaction: t })
+              .then(function(user) {
+                // console.log(user)
+              })
+              .catch(function(err) {
+                reject({ success: err });
+              });
+          })
+          .then(function(result) {
+            console.log("SUCCESS");
+            resolve({ success: true });
           })
           .catch(function(err) {
-            reject({ success: err });
+            reject({ success: "false3" });
           });
       })
-      .then(function(result) {
-        console.log("SUCCESS")
-        resolve({ success: true });
-      })
-      .catch(function(err) {
-        reject({ success: "false3" });
+      .catch(err => {
+        reject({ success: "false4" });
       });
-    })
-    .catch(err=>{
-      reject({success : "false4"});
-    })
   });
 };
-
 
 authenticationMethods.authenticateUser = function(username, password) {
   return new Promise(function(resolve, reject) {
@@ -135,7 +137,7 @@ authenticationMethods.authenticateUser = function(username, password) {
               const token = jwt.sign(
                 {
                   id: result.username,
-                  type: result.dataValues.type,
+                  type: result.dataValues.type
                 },
                 key,
                 { expiresIn: "1h" }
@@ -144,7 +146,7 @@ authenticationMethods.authenticateUser = function(username, password) {
               const user_id = result.dataValues.username;
               var decoded = jwt.decode(token, { complete: true });
               console.log(user_id + " " + type);
-              
+
               resolve({
                 success: true,
                 token: token,
@@ -169,7 +171,8 @@ authenticationMethods.authenticateUser = function(username, password) {
 
 authenticationMethods.authenticateStudent = function(username) {
   return new Promise(function(resolve, reject) {
-    models.student.findOne({
+    models.student
+      .findOne({
         where: {
           id: username
         }
@@ -177,35 +180,34 @@ authenticationMethods.authenticateStudent = function(username) {
       .then(result => {
         if (result) {
           const emailid = result.dataValues.email;
-           console.log(result.dataValues.email);
-            const otptoken = otpGenerator.generate(6, options);
-             console.log(otptoken);
-            var transporter = nodemailer.createTransport({
-              service: 'gmail',
-              auth: {
-                user: 'adithyarajagopal1999@gmail.com',
-                pass: 'password'
-              }
-            });
-            var mailOptions = {
-              from: 'adithyarajagopal1999@gmail.com',
-              to: emailid,
-              subject: "OTP",
-              // text: `Your one time password is ${otptoken}`
-              html: `<p>Your One-Time Password is <b>${otptoken}</b></p>`
+          console.log(result.dataValues.email);
+          const otptoken = otpGenerator.generate(6, options);
+          console.log(otptoken);
+          var transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+              user: "globalelective1@gmail.com",
+              pass: "global@123"
             }
-            transporter.sendMail(mailOptions, function(error, info){
-              if(error){
-                console.log(error);
-              }
-              else {
-                console.log('Email sent: ' + info.response);
-              }
-            })
-            resolve({
-                success: true,
-                token: otptoken,
-              });
+          });
+          var mailOptions = {
+            from: "globalelective1@gmail.com",
+            to: emailid,
+            subject: "OTP",
+            // text: `Your one time password is ${otptoken}`
+            html: `<p>Your One-Time Password is <b>${otptoken}</b></p>`
+          };
+          transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log("Email sent: " + info.response);
+            }
+          });
+          resolve({
+            success: true,
+            token: otptoken
+          });
         } else {
           reject(new Error());
         }
@@ -227,26 +229,26 @@ authenticationMethods.authenticateStud = function(username) {
       .then(result => {
         if (result) {
           // console.log(result)
-              console.log("correct password-bcrypt");
-              const token = jwt.sign(
-                {
-                  id: result.id,
-                  type: "student",
-                },
-                key,
-                { expiresIn: "1h" }
-              );
-              const type = "student";
-              const user_id = result.dataValues.id;
-              var decoded = jwt.decode(token, { complete: true });
-              console.log(user_id + " " + type);
-              
-              resolve({
-                success: true,
-                token: token,
-                type: type,
-                user_id: user_id
-              });
+          console.log("correct password-bcrypt");
+          const token = jwt.sign(
+            {
+              id: result.id,
+              type: "student"
+            },
+            key,
+            { expiresIn: "1h" }
+          );
+          const type = "student";
+          const user_id = result.dataValues.id;
+          var decoded = jwt.decode(token, { complete: true });
+          console.log(user_id + " " + type);
+
+          resolve({
+            success: true,
+            token: token,
+            type: type,
+            user_id: user_id
+          });
         } else {
           reject(new Error());
         }
