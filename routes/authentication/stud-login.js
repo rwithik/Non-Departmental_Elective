12eb59c87afe07891b7b3e1c99663b11ed13b244
rwithik <rwithik@gmail.com>
2020-01-13 15:08:42 +0530

    otp bugfix

--- routes/authentication/stud-login.js
+++ routes/authentication/stud-login.js
@@ -2,60 +2,51 @@ const express = require('express');
 const router = express.Router();
 const methods = require('../../methods')
 
-const models = require('../../models');
+let states = []
 
-const state = {
-    username: null,
-    token: null
-}
-
-router.post('/',(req,res)=>{
-    state.username = req.body.username;
+router.post('/', (req, res) => {
+    let state = {};
+    state.username = req.body.username.toUpperCase();
     methods.authentication.authenticateStudent(state.username)
-    .then(result => {
-        // console.log(result.token);
-        state.token = result.token;
-        res.render('otp',{user : state.username , msg : ""})
-    })
-    .catch(err => {
-       res.redirect("/?success=false")
-    })
+        .then(result => {
+            state.token = result.token;
+            res.render('otp', { user: state.username, msg: "" })
+            states.push(state);
+        })
+        .catch(err => {
+            res.redirect("/?success=false")
+        })
 })
 
-router.post('/confirmOtp',(req,res)=>{
-    // state.username = req.body.username;
-    // methods.authentication.authenticateStudent(state.username)
-    // .then(result => {
-    //     // console.log(result.token);
-    //     state.token = result.token;
-    // })
-    if(state.username === req.body.username && state.token === req.body.otp){
-        console.log("Inside post /login")
+router.post('/confirmOtp', (req, res) => {
+    console.log('states');
+    console.log(states);
+    const state = states.filter(i => i.username === req.body.username)[0]
+
+    if (state.token === req.body.otp) {
         username = req.body.username;
-        // req.userID = userID;
-        // req.
         methods.authentication.authenticateStud(username)
-        .then(result=>{
-            console.log("Logged in")
-            console.log(result.token)
-            req.token = result.token
-            req.session.token = result.token
-            if(result.type === "student"){
-                res.redirect("/student/dashboard")        
-            }
-              
-        })
-        .catch(err=>{
-           
-            console.log(err)
-            res.status(400).json({success : false})
-        })
+            .then(result => {
+                console.log("Logged in")
+                console.log(result.token)
+                req.token = result.token
+                req.session.token = result.token
+                states = states.filter(i => i.username !== username);
+                if (result.type === "student") {
+                    res.redirect("/student/dashboard")
+                }
+
+            })
+            .catch(err => {
+
+                console.log(err)
+                res.status(400).json({ success: false })
+            })
     }
     else {
         username = req.body.username
-        res.render('otp',{user : username , msg :"Incorrect OTP"})
+        res.render('otp', { user: username, msg: "Incorrect OTP" })
     }
 })
 
-module.exports.state = state;
 module.exports = router;
